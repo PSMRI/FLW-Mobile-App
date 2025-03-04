8a2de05b99f5ab6778a21c62ca34f0ae494bf864
Ritik <ritik.rathaur@bizbrolly.com>
2025-03-04 15:21:42 +0530

    Asha profile work done

---
+++ app/src/main/java/org/piramalswasthya/sakhi/ui/home_activity/asha_profile/AshaProfileFragment.kt
@@ -0,0 +1,131 @@
+package org.piramalswasthya.sakhi.ui.home_activity.asha_profile
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import org.piramalswasthya.sakhi.R
+import org.piramalswasthya.sakhi.adapters.FormInputAdapter
+import org.piramalswasthya.sakhi.databinding.FragmentAshaProfileBinding
+import org.piramalswasthya.sakhi.ui.home_activity.HomeActivity
+import org.piramalswasthya.sakhi.ui.home_activity.maternal_health.pregnant_woment_anc_visits.form.PwAncFormViewModel
+import org.piramalswasthya.sakhi.work.WorkerUtils
+
+
+@AndroidEntryPoint
+class AshaProfileFragment : Fragment() {
+
+    private var _binding: FragmentAshaProfileBinding? = null
+    private val binding: FragmentAshaProfileBinding
+        get() = _binding!!
+    private val viewModel: AshaProfileViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentAshaProfileBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.recordExists.observe(viewLifecycleOwner) { notIt ->
+
+            notIt?.let { recordExists ->
+                binding.fabEdit.visibility = if (recordExists) View.VISIBLE else View.GONE
+                binding.btnSubmit.visibility = if (recordExists) View.GONE else View.VISIBLE
+                val adapter = FormInputAdapter(
+                    formValueListener = FormInputAdapter.FormValueListener { formId, index ->
+                        viewModel.updateListOnValueChanged(formId, index)
+                        hardCodedListUpdate(formId)
+                    },
+                )
+                binding.form.rvInputForm.adapter = adapter
+                lifecycleScope.launch {
+                    viewModel.formList.collect {
+                        if (it.isNotEmpty())
+                            adapter.submitList(it)
+
+                    }
+                }
+            }
+        }
+
+        binding.fabEdit.setOnClickListener {
+            viewModel.setRecordExist(false)
+        }
+        binding.btnSubmit.setOnClickListener {
+            viewModel.saveForm()
+        }
+
+        viewModel.state.observe(viewLifecycleOwner) { state ->
+            when (state!!) {
+                AshaProfileViewModel.State.IDLE -> {
+                }
+
+                AshaProfileViewModel.State.SAVING -> {
+                    binding.llContent.visibility = View.GONE
+                    binding.pbForm.visibility = View.VISIBLE
+                }
+
+                AshaProfileViewModel.State.SAVE_SUCCESS -> {
+                    binding.llContent.visibility = View.VISIBLE
+                    binding.pbForm.visibility = View.GONE
+                    Toast.makeText(context, "Save Successful", Toast.LENGTH_LONG).show()
+                    findNavController().navigateUp()
+                }
+
+                AshaProfileViewModel.State.SAVE_FAILED -> {
+                    Toast.makeText(
+
+                        context, "Something wend wong! Contact testing!", Toast.LENGTH_LONG
+                    ).show()
+                    binding.llContent.visibility = View.VISIBLE
+                    binding.pbForm.visibility = View.GONE
+                }
+            }
+        }
+    }
+
+
+
+
+    private fun hardCodedListUpdate(formId: Int) {
+        binding.form.rvInputForm.adapter?.apply {
+            when (formId) {
+                1 -> {
+                    notifyItemChanged(1)
+                    notifyItemChanged(2)
+
+                }
+
+                4, 5 -> {
+                    notifyDataSetChanged()
+                }
+
+            }
+        }
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+        activity?.let {
+            (it as HomeActivity).updateActionBar(
+                R.drawable.ic__menopause,
+                getString(R.string.asha_profile)
+            )
+        }
+    }
+
+
+
+}
