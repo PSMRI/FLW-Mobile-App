5796b1c0f37f3d796ee41deacc42bfd5421a548b
Ritik <ritik.rathaur@bizbrolly.com>
2025-06-24 12:33:00 +0530

    FLW - 299 ABHA generation and mapping with ben

---
+++ app/src/main/java/org/piramalswasthya/sakhi/database/room/dao/ABHAGenratedDao.kt
@@ -0,0 +1,30 @@
+package org.piramalswasthya.sakhi.database.room.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Transaction
+import androidx.room.Update
+import org.piramalswasthya.sakhi.model.ABHAModel
+import org.piramalswasthya.sakhi.model.BenWithABHAGeneratedCache
+import org.piramalswasthya.sakhi.model.TBScreeningCache
+
+@Dao
+interface ABHAGenratedDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun saveABHA(abhaModel: ABHAModel)
+    @Update
+    suspend fun updateAbha(abhaModel: ABHAModel)
+
+    @Query("DELETE FROM ABHA_GENERATED WHERE benId = :benId")
+    suspend fun deleteAbhaByBenId(benId: Long)
+
+    @Query("SELECT * FROM ABHA_GENERATED WHERE benId = :benId")
+    suspend fun getAbhaByBenId(benId: Long): ABHAModel?
+
+    @Transaction
+    @Query("SELECT * FROM BEN_BASIC_CACHE WHERE benId = :benId")
+    suspend fun getBenWithAbha(benId: Long): BenWithABHAGeneratedCache?
+}
