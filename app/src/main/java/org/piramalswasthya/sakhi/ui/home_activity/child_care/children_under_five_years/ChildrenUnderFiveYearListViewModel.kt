271859ce9f63dd99319600ea6a207626fbb83aab
deepakkumar <kumar.deepak@bizbrolly.com>
2025-10-10 18:57:22 +0530

    Children under 5 years card listing implemented and data is set and navigation is also done

---
+++ app/src/main/java/org/piramalswasthya/sakhi/ui/home_activity/child_care/children_under_five_years/ChildrenUnderFiveYearListViewModel.kt
@@ -0,0 +1,46 @@
+package org.piramalswasthya.sakhi.ui.home_activity.child_care.children_under_five_years
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.launch
+import org.piramalswasthya.sakhi.helpers.filterBenList
+import org.piramalswasthya.sakhi.repositories.RecordsRepo
+import javax.inject.Inject
+
+
+@HiltViewModel
+class ChildrenUnderFiveYearListViewModel @Inject constructor(
+    recordsRepo: RecordsRepo
+) : ViewModel() {
+
+    private val allBenList = recordsRepo.childFilteredList
+    private val filter = MutableStateFlow("")
+    private val kind = MutableStateFlow("false")
+
+    val benList = allBenList.combine(kind) { list, kind ->
+        if (kind.equals("false", true)) {
+            filterBenList(list, false)
+        } else {
+            filterBenList(list, true)
+        }
+    }.combine(filter) { list, filter ->
+        filterBenList(list, filter)
+    }
+
+    fun filterText(text: String) {
+        viewModelScope.launch {
+            filter.emit(text)
+        }
+
+    }
+
+    fun filterType(type: String) {
+        viewModelScope.launch {
+            kind.emit(type)
+        }
+
+    }
+}
