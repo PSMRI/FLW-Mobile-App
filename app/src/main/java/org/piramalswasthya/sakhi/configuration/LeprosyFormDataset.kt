36ddbdd73a768cd7b861548616c37d380595c0b5
Nikhil Fegade <nikhilfegade17@gmail.com>
2025-11-27 13:12:08 +0530

    string file added v8

--- app/src/main/java/org/piramalswasthya/sakhi/configuration/LeprosyFormDataset.kt
+++ app/src/main/java/org/piramalswasthya/sakhi/configuration/LeprosyFormDataset.kt
@@ -86,7 +86,7 @@ class LeprosyFormDataset(
     private val leprosyStatus = FormElement(
         id = 8,
         inputType = InputType.DROPDOWN,
-        title = resources.getString(R.string.leprosy_status),
+        title = resources.getString(R.string.str_leprosy_status),
         arrayId = R.array.leprosy_status,
         entries = resources.getStringArray(R.array.leprosy_status),
         required = false,
