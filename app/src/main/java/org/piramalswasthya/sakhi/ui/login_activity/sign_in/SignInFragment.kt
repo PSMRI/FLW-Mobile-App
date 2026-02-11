af756b675d13e77c1eb9481bd46072879bfaea64
Sushant☯ <sushant.work021@gmail.com>
2026-02-11 17:27:48 +0530

    Removed commented code

--- app/src/main/java/org/piramalswasthya/sakhi/ui/login_activity/sign_in/SignInFragment.kt
+++ app/src/main/java/org/piramalswasthya/sakhi/ui/login_activity/sign_in/SignInFragment.kt
@@ -243,58 +243,6 @@ class SignInFragment : Fragment() {
                         }
                     }
 
-//                    if (prefDao.getLoggedInUser()?.role.equals("asha", true)) {
-//                        WorkerUtils.triggerGenBenIdWorker(requireContext())
-//                        if (BuildConfig.FLAVOR.equals("niramay", true))  {
-//                            if (viewModel.getLoggedInUser()?.serviceMapId == 1718 ||
-//                                //Below ServiceMapId are form Indian Oil Project
-//                                viewModel.getLoggedInUser()?.serviceMapId ==1722 ||
-//                                viewModel.getLoggedInUser()?.serviceMapId ==1723 ||
-//                                viewModel.getLoggedInUser()?.serviceMapId ==1724){
-//                                findNavController().navigate(
-//                                    if (prefDao.getLocationRecord() == null) SignInFragmentDirections.actionSignInFragmentToServiceLocationActivity()
-//                                    else SignInFragmentDirections.actionSignInFragmentToHomeActivity())
-//                                activity?.finish()
-//                            }else{
-//                                binding.clContent.visibility = View.VISIBLE
-//                                binding.pbSignIn.visibility = View.GONE
-//                                binding.tvError.visibility = View.GONE
-//                                Toast.makeText(requireContext(),"This user is not from Niramay Project",Toast.LENGTH_SHORT).show()
-//                            }
-//
-//                        }else if(BuildConfig.FLAVOR.equals("xushrukha", true)){
-//                            if (viewModel.getLoggedInUser()?.serviceMapId == 1716){
-//                                findNavController().navigate(
-//                                    if (prefDao.getLocationRecord() == null) SignInFragmentDirections.actionSignInFragmentToServiceLocationActivity()
-//                                    else SignInFragmentDirections.actionSignInFragmentToHomeActivity())
-//                                activity?.finish()
-//                            }else{
-//                                binding.clContent.visibility = View.VISIBLE
-//                                binding.pbSignIn.visibility = View.GONE
-//                                binding.tvError.visibility = View.GONE
-//                                Toast.makeText(requireContext(),"This user is not from Xushrukha Project",Toast.LENGTH_SHORT).show()
-//                            }
-//
-//                        }else{
-//                            findNavController().navigate(
-//                                if (prefDao.getLocationRecord() == null) SignInFragmentDirections.actionSignInFragmentToServiceLocationActivity()
-//                                else SignInFragmentDirections.actionSignInFragmentToHomeActivity())
-//                            activity?.finish()
-//                        }
-//                    } else {
-//                        val locationRecord = LocationRecord(
-//                            LocationEntity(1, "India"),
-//                            LocationEntity(prefDao.getLoggedInUser()!!.state.id, prefDao.getLoggedInUser()!!.state.name),
-//                            LocationEntity(0, ""),
-//                            LocationEntity(prefDao.getLoggedInUser()!!.block.id, prefDao.getLoggedInUser()!!.block.name),
-//                            LocationEntity(prefDao.getLoggedInUser()!!.villages[0].id, prefDao.getLoggedInUser()!!.villages[0].name),
-//                        )
-//                        prefDao.saveLocationRecord(locationRecord)
-//
-//                        activity?.finish()
-//                        val goToHome = Intent(requireContext(), SupervisorActivity::class.java)
-//                        startActivity(goToHome)
-//                    }
                 }
             }
         }
