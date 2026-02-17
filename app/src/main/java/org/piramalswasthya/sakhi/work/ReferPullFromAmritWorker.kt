f6d47daf8b2d4002099d9fa4e22e307752194343
Mithun James <1007084+drtechie@users.noreply.github.com>
2026-02-17 12:26:48 +0530

    fix: added missing foreground service

--- app/src/main/java/org/piramalswasthya/sakhi/work/ReferPullFromAmritWorker.kt
+++ app/src/main/java/org/piramalswasthya/sakhi/work/ReferPullFromAmritWorker.kt
@@ -1,14 +1,18 @@
 package org.piramalswasthya.sakhi.work
 
 import android.content.Context
+import android.content.pm.ServiceInfo
+import android.os.Build
+import androidx.core.app.NotificationCompat
 import androidx.hilt.work.HiltWorker
 import androidx.work.CoroutineWorker
+import androidx.work.ForegroundInfo
 import androidx.work.WorkerParameters
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedInject
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import org.piramalswasthya.sakhi.repositories.CbacRepo
+import org.piramalswasthya.sakhi.R
 import org.piramalswasthya.sakhi.repositories.NcdReferalRepo
 import timber.log.Timber
 
@@ -22,7 +26,10 @@ class ReferPullFromAmritWorker @AssistedInject constructor(
         const val name = "refer-Pull"
     }
 
+    override suspend fun getForegroundInfo(): ForegroundInfo = createForegroundInfo()
+
     override suspend fun doWork(): Result {
+        try { setForeground(createForegroundInfo()) } catch (_: Throwable) {}
         return withContext(Dispatchers.IO) {
             return@withContext try {
                 val getNumPages: Int = referalRepo.pullAndPersistReferRecord()
@@ -37,5 +44,17 @@ class ReferPullFromAmritWorker @AssistedInject constructor(
                 Result.failure()
             }
         }
+    }
+
+    private fun createForegroundInfo(): ForegroundInfo {
+        val notification = NotificationCompat.Builder(
+            applicationContext,
+            applicationContext.getString(R.string.notification_sync_channel_id)
+        ).setContentTitle("Data Sync").setContentText("Downloading Referral Data")
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setProgress(100, 0, true).setOngoing(true).build()
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            ForegroundInfo(1003, notification, ServiceInfo.FOREGROUND_SERVICE_TYPE_DATA_SYNC)
+        } else ForegroundInfo(1003, notification)
     }
 }
