58659656be69b23bea3661b23b75f81e73991b8b
ritik-bizbrolly <ritik.rathaur@bizbrolly.com>
2026-03-02 12:16:26 +0530

    managed supervisor roles

--- app/src/main/java/org/piramalswasthya/sakhi/ui/asha_supervisor/profile/SupervisorProfileFragment.kt
+++ app/src/main/java/org/piramalswasthya/sakhi/ui/asha_supervisor/profile/SupervisorProfileFragment.kt
@@ -1,13 +1,20 @@
 package org.piramalswasthya.sakhi.ui.asha_supervisor.profile
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import dagger.hilt.android.AndroidEntryPoint
 import org.piramalswasthya.sakhi.R
 import org.piramalswasthya.sakhi.databinding.FragmentSupervisorProfileBinding
+import org.piramalswasthya.sakhi.helpers.ImageUtils
+import org.piramalswasthya.sakhi.ui.login_activity.LoginActivity
+import org.piramalswasthya.sakhi.work.WorkerUtils
+import kotlin.getValue
 import org.piramalswasthya.sakhi.ui.asha_supervisor.SupervisorActivity
 
 @AndroidEntryPoint
@@ -16,6 +23,25 @@ class SupervisorProfileFragment : Fragment()  {
     private var _binding: FragmentSupervisorProfileBinding? = null
     private val binding: FragmentSupervisorProfileBinding
         get() = _binding!!
+
+    private val viewModel: SupervisorProfileViewModel by viewModels()
+
+    private val logoutAlert by lazy {
+        var str = ""
+        str += resources.getString(R.string.are_you_sure_to_logout)
+
+        MaterialAlertDialogBuilder(requireActivity()).setTitle(resources.getString(R.string.logout))
+            .setMessage(str)
+            .setPositiveButton(resources.getString(R.string.yes)) { dialog, _ ->
+                viewModel.logout()
+                ImageUtils.removeAllBenImages(requireActivity())
+                WorkerUtils.cancelAllWork(requireActivity())
+                dialog.dismiss()
+            }.setNegativeButton(resources.getString(R.string.no)) { dialog, _ ->
+                dialog.dismiss()
+            }.create()
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,6 +55,32 @@ class SupervisorProfileFragment : Fragment()  {
         _binding = FragmentSupervisorProfileBinding.inflate(inflater, container, false)
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.tvValue.setText(viewModel.getUserMobile())
+        binding.emailtvValue.setText(viewModel.getUserEmail())
+        binding.supervisorId.setText("ID : ${ viewModel.getsupervisorId().toString()}")
+        binding.supervisorName.setText(viewModel.getSuperVisorname())
+        binding.subName.setText(viewModel.getSuperVisorSubname())
+        binding.districtvValue.text = viewModel.getDistrict()
+        binding.blocktvValue.text = viewModel.getBlock()
+        binding.subcentertvValue.text = viewModel.getSubcenter()
+        binding.logoutlayout.setOnClickListener {
+
+            logoutAlert.show()
+        }
+
+        viewModel.navigateToLoginPage.observe(viewLifecycleOwner) {
+            viewModel.navigateToLoginPage.observe(this) {
+                if (it) {
+                    startActivity(Intent(requireActivity(), LoginActivity::class.java))
+                    viewModel.navigateToLoginPageComplete()
+                    activity?.finish()
+                }
+            }
+        }
     }
     override fun onDestroyView() {
         super.onDestroyView()
