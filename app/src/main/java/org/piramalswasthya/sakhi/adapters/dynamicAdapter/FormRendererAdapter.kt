509d2b5caa730a8c2740524daad78b8d423ddd04
Kunalbizbrolly <kunal.bhatt@bizbrolly.com>
2026-04-09 13:47:10 +0530

    check_sam- crash fixes

--- app/src/main/java/org/piramalswasthya/sakhi/adapters/dynamicAdapter/FormRendererAdapter.kt
+++ app/src/main/java/org/piramalswasthya/sakhi/adapters/dynamicAdapter/FormRendererAdapter.kt
@@ -99,6 +99,11 @@
             super.onViewRecycled(holder)
         }
 
+        override fun onViewDetachedFromWindow(holder: FormViewHolder) {
+            holder.clear()
+            super.onViewDetachedFromWindow(holder)
+        }
+
         override fun getItemCount(): Int = fields.size
 
         inner class FormViewHolder(view: View) : RecyclerView.ViewHolder(view) {
@@ -188,6 +193,8 @@
             }
 
             fun clear() {
+                muacDebounceJob?.cancel()
+                muacDebounceJob = null
                 viewHolderScope.coroutineContext.cancelChildren()
             }
 
@@ -505,7 +512,7 @@
                                     field.value = value
                                     if (field.fieldId.contains("muac", ignoreCase = true)) {
                                         muacDebounceJob?.cancel()
-                                        muacDebounceJob = CoroutineScope(Dispatchers.Main).launch {
+                                        muacDebounceJob = viewHolderScope.launch {
                                             delay(1500)
                                             onValueChanged(field, value)
                                         }
