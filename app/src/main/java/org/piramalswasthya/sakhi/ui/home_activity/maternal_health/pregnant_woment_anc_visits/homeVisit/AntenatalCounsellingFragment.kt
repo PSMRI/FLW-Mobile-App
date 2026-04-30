b480fd227f55f1c2c523f04d6703cc75372c06ec
Nikhil Fegade <nikhilfegade17@gmail.com>
2026-04-30 18:49:16 +0530

    FLW-954

--- app/src/main/java/org/piramalswasthya/sakhi/ui/home_activity/maternal_health/pregnant_woment_anc_visits/homeVisit/AntenatalCounsellingFragment.kt
+++ app/src/main/java/org/piramalswasthya/sakhi/ui/home_activity/maternal_health/pregnant_woment_anc_visits/homeVisit/AntenatalCounsellingFragment.kt
@@ -84,7 +84,7 @@ class AntenatalCounsellingFragment : Fragment() {
     private var hasAnyDangerSign = false
     private var isSelectAllChecked = false
     private var lmpDate :Long? =null
-    private var referralForReason = resources.getString(R.string.str_suspected_high_risk_pregnancy)
+   // private var referralForReason = getString(R.string.str_suspected_high_risk_pregnancy)
     private var referType = "Maternal"
 
     override fun onCreateView(
@@ -193,7 +193,7 @@ class AntenatalCounsellingFragment : Fragment() {
             visitNumber = args.visitNumber
 
             binding.btnSave.isVisible = false
-            binding.tvLastVisitValue.text = visitDate
+            binding.tvLastVisitValue.text = visitDate?:"NA"
             binding.cbSelectAll.isVisible = false
             viewModel.loadFormSchema(benId, ANC_FORM_ID, visitDate, true, langCode,visitNumber)
             lifecycleScope.launch {
