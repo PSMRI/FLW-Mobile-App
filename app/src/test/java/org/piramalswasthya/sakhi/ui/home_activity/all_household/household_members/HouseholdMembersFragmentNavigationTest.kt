900bb13e4ebaa06f0aed5e32d43589fe756473b4
ishwarchouhan07 <Ishwar.Chouhan@piramalswasthya.org>
2026-08-31 17:09:52 +0530

    Coverage instruction console report (#603)
    * add console report
    
    * fix: restore required nav args
    
    * fix pw view form issue
    
    * fix system and github conflict
    
    * fix system and github conflict2

--- app/src/test/java/org/piramalswasthya/sakhi/ui/home_activity/all_household/household_members/HouseholdMembersFragmentNavigationTest.kt
+++ app/src/test/java/org/piramalswasthya/sakhi/ui/home_activity/all_household/household_members/HouseholdMembersFragmentNavigationTest.kt
@@ -32,7 +32,21 @@ class HouseholdMembersFragmentNavigationTest {
         unmockkConstructor(Bundle::class)
     }
 
-    private fun args() = HouseholdMembersFragmentArgs(hhId = 10L, fromDisease = 0, diseaseType = "v3")
+    private fun args(): HouseholdMembersFragmentArgs {
+        val ctor = HouseholdMembersFragmentArgs::class.java.declaredConstructors
+            .filterNot { c -> c.parameterTypes.any { it.simpleName == "DefaultConstructorMarker" } }
+            .maxByOrNull { it.parameterCount }!!
+        val values = ctor.parameterTypes.map { type ->
+            when (type) {
+                java.lang.Long.TYPE, java.lang.Long::class.java -> 10L
+                Integer.TYPE, Integer::class.java -> 0
+                String::class.java -> "v3"
+                else -> null
+            }
+        }.toTypedArray()
+        ctor.isAccessible = true
+        return ctor.newInstance(*values) as HouseholdMembersFragmentArgs
+    }
 
     @Test
     fun constructor_exposesEveryArgument() {
